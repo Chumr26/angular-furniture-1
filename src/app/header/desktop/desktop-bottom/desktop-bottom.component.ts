c76665dbf547d6262c83931668c76a578d344be6
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2025-05-25 17:07:15 +0700

    css refactor (header > desktop > bottom)

--- src/app/header/desktop/desktop-bottom/desktop-bottom.component.ts
+++ src/app/header/desktop/desktop-bottom/desktop-bottom.component.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 interface CategoryMenuItem {
-    label: string;
-    icon: string;
-    link: string;
+  label: string;
+  icon: string;
+  link: string;
 }
 @Component({
   selector: '[desktop-bottom]',
   imports: [],
   templateUrl: './desktop-bottom.component.html',
-  styleUrl: './desktop-bottom.component.css'
+  styleUrl: './desktop-bottom.component.css',
+  encapsulation: ViewEncapsulation.None, // Disable encapsulation
 })
 export class DesktopBottomComponent {
-    categories: CategoryMenuItem[] = [
-        { label: 'Chairs', icon: 'chair-icon', link: 'chairs' },
-        { label: 'Storage', icon: 'storage-icon', link: 'storage' },
-        { label: 'Armchairs', icon: 'armchair-icon', link: 'armchairs' },
-        { label: 'Sofas', icon: 'sofa-icon', link: 'sofas' },
-        { label: 'Beds', icon: 'bed-icon', link: 'beds' },
-        { label: 'Tables', icon: 'table-icon', link: 'tables' },
-        { label: 'Decor', icon: 'decor-icon', link: 'decor' },
-    ];
+  categories: CategoryMenuItem[] = [
+    { label: 'Chairs', icon: 'chair-icon', link: 'chairs' },
+    { label: 'Storage', icon: 'storage-icon', link: 'storage' },
+    { label: 'Armchairs', icon: 'armchair-icon', link: 'armchairs' },
+    { label: 'Sofas', icon: 'sofa-icon', link: 'sofas' },
+    { label: 'Beds', icon: 'bed-icon', link: 'beds' },
+    { label: 'Tables', icon: 'table-icon', link: 'tables' },
+    { label: 'Decor', icon: 'decor-icon', link: 'decor' },
+  ];
 }
