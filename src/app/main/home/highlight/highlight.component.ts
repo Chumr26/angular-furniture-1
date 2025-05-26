d1a9a006b9ba28454d3cc9fba6da50ef00aa0d9c
Chumr26 <nguyenanhkhoa26092002@gmail.com>
2025-05-26 12:46:08 +0700

    set up (main>home>highlight)

---
+++ src/app/main/home/highlight/highlight.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[highlight]',
+  imports: [],
+  templateUrl: './highlight.component.html',
+  styleUrl: './highlight.component.css'
+})
+export class HighlightComponent {
+
+}
--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HighlightComponent } from './highlight/highlight.component';
 
 @Component({
   selector: '[home]',
-  imports: [],
+  imports: [HighlightComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
